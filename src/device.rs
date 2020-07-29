2e9aa92f513bbd3d39b5fb4ab20677bc1f9a45aa
Will Page <compenguy@gmail.com>
2020-07-29 16:37:13 -0700

    Switch to HAL for efc init, remove some dead source files

--- src/device.rs
+++ src/device.rs
@@ -2,12 +2,10 @@ use atsam3x8e;
 use atsam3x8e_hal as hal;
 
 pub(crate) mod led;
-//pub(crate) mod pio;
 //pub(crate) mod time;
 //pub(crate) mod serial;
 //pub(crate) mod spi;
 //pub(crate) mod usb;
-pub(crate) mod sys;
 pub(crate) mod wdt;
 
 pub(crate) struct Device<C, W, PB> {
@@ -26,9 +24,15 @@ impl Due {
             SUPC: supc,
             WDT: wdt,
             PIOB: piob,
+            EFC0: efc0,
+            EFC1: efc1,
             ..
         } = atsam3x8e::Peripherals::take().expect("Failed to acquire device peripherals");
         let clk = hal::clock::SystemClocks::new(pmc, supc);
+        //let clk = hal::clock::SystemClocks::with_plla_clk(pmc, supc);
+        // init embedded flash controllers
+        let _efc0 = hal::flash::FlashController0::new(efc0);
+        let _efc1 = hal::flash::FlashController1::new(efc1);
 
         let mut d = Device { clk, wdt, piob };
         d._init();
@@ -39,7 +43,6 @@ impl Due {
     // https://github.com/arduino/ArduinoModule-CMSIS-Atmel/blob/master/CMSIS-Atmel/CMSIS/Device/ATMEL/sam3xa/source/system_sam3xa.c
     // for sys_init steps
     fn _init(&mut self) {
-        //self.init_flash_rw_cycle_times();
         self.disable_watchdog();
     }
 }
--- src/device/sys.rs
+++
@@ -1,9 +0,0 @@
-use crate::device::Due;
-
-impl Due {
-    pub(crate) fn init_flash_rw_cycle_times(&mut self) {
-        // Set FWS according to SYS_BOARD_MCKR configuration
-        //self.p.EFC0.fmr.write(|w| unsafe { w.fws().bits(4) });
-        //self.p.EFC1.fmr.write(|w| unsafe { w.fws().bits(4) });
-    }
-}
